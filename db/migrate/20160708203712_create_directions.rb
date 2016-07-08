c91addc86963b346f4bb3be1003026080fbd7326
Vadim Sokoltsov <vforvad@gmail.com>
2016-07-08 23:43:36 +0300

    add migration + add model

--- db/migrate/20160708203712_create_directions.rb
+++ db/migrate/20160708203712_create_directions.rb
@@ -1,6 +1,9 @@
 class CreateDirections < ActiveRecord::Migration
   def change
     create_table :directions do |t|
+      t.string :title, index: true, null: false
+      t.text :description, index: true, null: false
+      t.timestamps
     end
   end
 end
