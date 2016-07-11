5628a23f1dd070fcb3f1a62572f9988495929b72
Vadim Sokoltsov <vforvad@gmail.com>
2016-07-12 02:09:54 +0300

    add create aciton

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'active_model_serializers'
 gem 'rack-cors', require: 'rack/cors'
+gem 'pry-rails'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/api/v1/directions_controller.rb
+++ app/controllers/api/v1/directions_controller.rb
@@ -3,4 +3,13 @@ class Api::V1::DirectionsController < Api::ApiController
     directions = Direction.all
     render json: directions
   end
+
+  def create
+    direction = Direction.new(params[:direction].symbolize_keys)
+    if direction.save
+      render json: { direction: direction }
+    else
+      render json: { errors: direction.errors }, status: :uprocessable_entity
+    end
+  end
 end
