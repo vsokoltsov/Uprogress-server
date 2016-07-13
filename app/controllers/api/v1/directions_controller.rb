5de5227f1654ad99eed6912255f6c3cb40f8a134
Vadim Sokoltsov <vforvad@gmail.com>
2016-07-14 01:59:35 +0300

    add show and update actions for directions_controller

--- app/controllers/api/v1/directions_controller.rb
+++ app/controllers/api/v1/directions_controller.rb
@@ -12,4 +12,19 @@ class Api::V1::DirectionsController < Api::ApiController
       render json: { errors: form.errors }, status: :uprocessable_entity
     end
   end
+
+  def show
+    direction = Direction.find(params[:id])
+    render json: { direction: direction }
+  end
+
+  def update
+    direction = Direction.find(params[:id])
+    form = Form::Direction.new(direction, params[:direction])
+    if form.submit
+      render json: { direction: form.object }
+    else
+      render json: { errors: form.errors }, status: :uprocessable_entity
+    end
+  end
 end
