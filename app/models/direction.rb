ded6033971417782c539931c6a8382497a3701ca
Vadim Sokoltsov <vforvad@gmail.com>
2016-07-28 01:27:24 +0300

    implement calculating percents for steps

--- app/models/direction.rb
+++ app/models/direction.rb
@@ -1,3 +1,9 @@
 class Direction < ActiveRecord::Base
   has_many :steps
+
+  def percents_result
+    steps_count = steps.size
+    completed_steps = steps.completed.size
+    completed_steps / steps_count.to_f * 100 if steps.present?
+  end
 end
--- app/models/step.rb
+++ app/models/step.rb
@@ -1,3 +1,6 @@
 class Step < ActiveRecord::Base
   belongs_to :directions
+
+  scope :active, -> { where(is_done: false) }
+  scope :completed, -> { where(is_done: true) }
 end
