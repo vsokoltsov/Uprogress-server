49a7fd8a53f46a1b5418abf596081f0f023033cf
Vadim Sokoltsov <vforvad@gmail.com>
2016-08-21 01:23:38 +0300

    add custom error class and add modules into base api controller

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,4 +1,23 @@
 # frozen_string_literal: true
 class Api::ApiController < ApplicationController
-  protect_from_forgery with: :null_session
+  include JsonWebToken
+  include SessionConcern
+
+  rescue_from Api::Error do |e|
+    error_response e, e.status, e.addition
+  end
+
+  private
+
+  def error_response(exception, code, addition = false)
+    obj = { error: exception.message }
+
+    obj[:description] = addition if addition
+    if Rails.env.development?
+      obj[:exception] = exception.class.name
+      obj[:backtrace] = exception.backtrace
+    end
+    render json: obj, status: code
+  end
+
 end
---
+++ app/exceptions/api.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+module Api
+  class Error < ::Exception
+
+    attr_reader :status
+    attr_reader :addition
+
+    def initialize(message, status, addition = false)
+      super(message)
+      @status = status
+      @addition = addition
+    end
+  end
+end
