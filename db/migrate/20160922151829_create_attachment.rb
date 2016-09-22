28edf12d4e60ea991f14ad250332e21678cf07af
Vadim Sokoltsov <vforvad@gmail.com>
2016-09-22 18:23:36 +0300

    add migration and model for attachments

---
+++ db/migrate/20160922151829_create_attachment.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+class CreateAttachment < ActiveRecord::Migration
+  def change
+    create_table :attachments do |t|
+      t.integer :attachable_id, index: true
+      t.string :attachable_type, index: true
+      t.string :file
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160921222523) do
+ActiveRecord::Schema.define(version: 20160922151829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "attachments", force: :cascade do |t|
+    t.integer  "attachable_id"
+    t.string   "attachable_type"
+    t.string   "file"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "attachments", ["attachable_id"], name: "index_attachments_on_attachable_id", using: :btree
+  add_index "attachments", ["attachable_type"], name: "index_attachments_on_attachable_type", using: :btree
 
   create_table "authorizations", force: :cascade do |t|
     t.integer  "user_id"
