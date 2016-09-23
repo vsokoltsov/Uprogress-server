84197e173f64c5dc751fe690dc492ee9bfbf565e
Vadim Sokoltsov <vforvad@gmail.com>
2016-09-23 19:08:53 +0300

    add two statistic queries to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,8 +1,21 @@
 # frozen_string_literal: true
 class UserSerializer < ActiveModel::Serializer
   root 'user'
-  attributes :id, :nick, :first_name, :last_name, :description, :location
+  attributes :id, :nick, :first_name, :last_name, :description, :location,
+             :finished_directions, :new_directions
 
   has_many :directions, serializer: DirectionsSerializer
   has_one :attachment, serializer: AttachmentSerializer
+
+  def finished_directions
+    scope.directions.select do |attr|
+      attr.steps.map(&:is_done).uniq.length == 1
+    end
+  end
+
+  def new_directions
+    scope.directions.select do |attr|
+      attr.steps.blank?
+    end
+  end
 end
