2cd5e2ca3d373e2cf0fc91ec313e626ea74264fc
Vadim Sokoltsov <vforvad@gmail.com>
2016-10-09 17:43:56 +0300

    remove attribute

--- app/serializers/statistics_serializer.rb
+++ app/serializers/statistics_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class StatisticsSerializer < ActiveModel::Serializer
   root 'statistics'
-  attributes :directions, :steps, :directions_steps
+  attributes :directions, :steps
 
   def directions
     directions_count = object.directions.size.to_f
