d7683578892865497ca8abee253be15a34940b0b
Vadim Sokoltsov <vforvad@gmail.com>
2016-10-20 23:32:23 +0300

    change S3 region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
   config.aws_credentials = {
     access_key_id:     ENV['S3_KEY'],
     secret_access_key: ENV['S3_SECRET'],
-    region: 'eu-central-1'
+    region: 'us-west-1'
   }
 
 end
