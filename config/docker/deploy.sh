92f8ead73d5b811e8b77cf95443bb3aa1677da39
Vadim Sokoltsov <vforvad@gmail.com>
2016-11-07 00:31:00 +0300

    update an ssh script for deploying vi docker

--- config/docker/deploy.sh
+++ config/docker/deploy.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 DOCKER_IMAGE="vsokoltsov/uprogress"
+HOST_IP="52.90.29.157"
+DOCKER_CONFIG_SCRIPT="docker_config.sh"
 
 echo "DOCKER DEPLOY"
 
 # BUILD NEW IMAGE
-docker build -t "$DOCKER_IMAGE:latest" .
+# docker build -t "$DOCKER_IMAGE:latest" .
 
 #PUSH TO DOCKER HUB
-docker push "$DOCKER_IMAGE:latest"
+# docker push "$DOCKER_IMAGE:latest"
+
+#COPY docker_compose file
+scp "$(pwd)/docker-compose.yml" "ec2-user@$HOST_IP:/home/ec2-user"
+
+#CONNECT_TO SSH AND RUN SCRIPT
+ssh "ec2-user@$HOST_IP" 'bash -s' < "$(pwd)/config/docker/$DOCKER_CONFIG_SCRIPT"
