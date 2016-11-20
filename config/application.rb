b39ab2922706f20c988aa1a850f31f1bc221c80c
Vadim Sokoltsov <vforvad@gmail.com>
2016-11-20 12:57:08 +0300

    add some modifications for travis.ci config

--- config/application.rb
+++ config/application.rb
@@ -26,8 +26,8 @@ module UprogressServer
     config.active_record.raise_in_transactional_callbacks = true
     config.middleware.insert_before 0, 'Rack::Cors' do
       allow do
-        origins 'localhost:9000'
-        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete]
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
       end
     end
 
