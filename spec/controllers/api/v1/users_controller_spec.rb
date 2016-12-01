6f6766afd4a7798e6ffd65e0eeed3245fd621266
Vadim Sokoltsov <vforvad@gmail.com>
2016-12-01 23:07:23 +0300

    add boilerplates for the future specs

---
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+describe Api::V1::UsersController do
+
+end
---
+++ spec/models/form/session_spec.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+describe Form::Session do
+end
