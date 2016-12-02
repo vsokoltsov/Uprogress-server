7ea1882ec054b759737e3ccbde5c78a56aae296d
Vadim Sokoltsov <vforvad@gmail.com>
2016-12-02 16:54:19 +0300

    add specs for user form object

--- spec/models/form/user_spec.rb
+++ spec/models/form/user_spec.rb
@@ -2,4 +2,36 @@
 require 'rails_helper'
 
 describe Form::User do
+  let!(:user) { create :user }
+  let!(:user_attrs) do
+    {
+      email: 'example@mail.com',
+      first_name: 'Ololo',
+      last_name: 'Oloshevich'
+    }
+  end
+
+  describe '#submit' do
+    context 'with valid attributes' do
+      let!(:form) { ::Form::User.new(user, user_attrs) }
+
+      it 'update user name' do
+        form.submit
+        user.reload
+        expect(user.first_name).to eq(user_attrs[:first_name])
+        expect(user.last_name).to eq(user_attrs[:last_name])
+      end
+    end
+
+    context 'with invalid attributes' do
+      let!(:form) { ::Form::User.new(user, {}) }
+
+      it 'doesn\'t update user name' do
+        form.submit
+        user.reload
+        expect(user.first_name).to eq(nil)
+        expect(user.last_name).to eq(nil)
+      end
+    end
+  end
 end
