e2694745ef252195cf803be307af5cfcaeaa8c6b
Vadim Sokoltsov <vforvad@gmail.com>
2016-12-06 11:36:31 +0300

    remove code duplications from controllers

--- app/controllers/api/v1/directions_controller.rb
+++ app/controllers/api/v1/directions_controller.rb
@@ -9,15 +9,11 @@ class Api::V1::DirectionsController < Api::ApiController
   end
 
   def create
-    form = Form::Direction.new(
+    @form = Form::Direction.new(
       current_user.directions.build,
       params[:direction].to_unsafe_hash
     )
-    if form.submit
-      render json: form.object, serializer: DirectionSerializer
-    else
-      render json: { errors: form.errors }, status: :unprocessable_entity
-    end
+    handle_form_submit!
   end
 
   def show
@@ -27,11 +23,17 @@ class Api::V1::DirectionsController < Api::ApiController
 
   def update
     direction = current_user.directions.find(params[:id])
-    form = Form::Direction.new(direction, params[:direction].to_unsafe_hash)
-    if form.submit
-      render json: form.object, serializer: DirectionSerializer
+    @form = Form::Direction.new(direction, params[:direction].to_unsafe_hash)
+    handle_form_submit!
+  end
+
+  private
+
+  def handle_form_submit!
+    if @form.submit
+      render json: @form.object, serializer: DirectionSerializer
     else
-      render json: { errors: form.errors }, status: :unprocessable_entity
+      render json: { errors: @form.errors }, status: :unprocessable_entity
     end
   end
 end
--- app/controllers/api/v1/steps_controller.rb
+++ app/controllers/api/v1/steps_controller.rb
@@ -4,26 +4,28 @@ class Api::V1::StepsController < Api::ApiController
 
   def create
     direction = Direction.find(params[:direction_id])
-    form = Form::Step.new(direction.steps.build, params[:step].to_unsafe_hash)
-    if form.submit
-      render json: form.object, serializer: StepSerializer
-    else
-      render json: { errors: form.errors }, status: :unprocessable_entity
-    end
+    @form = Form::Step.new(direction.steps.build, params[:step].to_unsafe_hash)
+    handle_form_submit!
   end
 
   def update
     step = Step.find(params[:id])
-    form = Form::Step.new(step, params[:step].to_unsafe_hash)
-    if form.submit
-      render json: form.object, serializer: StepSerializer
-    else
-      render json: { errors: form.errors }, status: :unprocessable_entity
-    end
+    @form = Form::Step.new(step, params[:step].to_unsafe_hash)
+    handle_form_submit!
   end
 
   def destroy
     step = Step.find(params[:id])
     render json: step, serializer: StepSerializer if step.destroy
+  end
+
+  private
+
+  def handle_form_submit!
+    if @form.submit
+      render json: @form.object, serializer: StepSerializer
+    else
+      render json: { errors: @form.errors }, status: :unprocessable_entity
+    end
   end
 end
