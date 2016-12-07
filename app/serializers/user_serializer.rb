7b0dae34888b5edee3c2face3dd93b365b85a434
Vadim Sokoltsov <vforvad@gmail.com>
2016-12-07 21:14:01 +0300

    move logs queries to separated object

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -4,35 +4,21 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :nick, :first_name, :last_name, :description, :location,
              :finished_directions, :new_directions, :recent_actions
 
-  delegate :directions, :finished_directions, :new_directions, to: :query_scope
+  delegate :directions, :finished_directions, :new_directions, to: :directions_scope
+  delegate :recent_actions, to: :logs_scope
 
   has_many :directions, serializer: DirectionsSerializer
   has_one :attachment, serializer: AttachmentSerializer
 
-  def query_scope
-    @query_scope ||= UserDirectionsScope.new(object)
+  def directions_scope
+    @directions ||= UserDirectionsScope.new(object)
   end
 
-  def recent_actions
-    logs = sorted_logs(object).group_by do |item|
-      item.created_at.to_date
-    end
-
-    Hash[*logs.flatten]
+  def logs_scope
+    @logs_scope ||= LogsScope.new(object)
   end
 
   def json_key
     'user'
-  end
-
-  private
-
-  def sorted_logs(object)
-    object.system_logs
-          .order(created_at: :desc)
-          .limit(5)
-          .sort do |a, b|
-            b.created_at <=> a.created_at
-          end
   end
 end
