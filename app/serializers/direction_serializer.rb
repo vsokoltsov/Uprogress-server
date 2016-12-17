f2d5b7ebe75ac1614b80cff633332f94f2b1ab09
Vadim Sokoltsov <vforvad@gmail.com>
2016-12-17 16:55:59 +0300

    change serializer due to new fields

--- app/serializers/direction_serializer.rb
+++ app/serializers/direction_serializer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class DirectionSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :percents_result
+  attributes :id, :title, :description, :steps_count, :finished_steps_count, :percents_result
 
   delegate :percents_result, to: :object
   has_many :steps
--- app/serializers/directions_serializer.rb
+++ app/serializers/directions_serializer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class DirectionsSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :percents_result
+  attributes :id, :title, :description, :steps_count, :finished_steps_count, :percents_result
 
   delegate :percents_result, to: :object
 end
