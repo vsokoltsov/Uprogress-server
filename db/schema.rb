849dfc7e7c5027438741bd20cf6f1a16c1241930
Vadim Sokoltsov <vforvad@gmail.com>
2016-12-17 16:53:01 +0300

    add steps_count and finished_steps_count columns

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160923132025) do
+ActiveRecord::Schema.define(version: 20161217103731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,10 +21,9 @@ ActiveRecord::Schema.define(version: 20160923132025) do
     t.string   "file"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["attachable_id"], name: "index_attachments_on_attachable_id", using: :btree
+    t.index ["attachable_type"], name: "index_attachments_on_attachable_type", using: :btree
   end
-
-  add_index "attachments", ["attachable_id"], name: "index_attachments_on_attachable_id", using: :btree
-  add_index "attachments", ["attachable_type"], name: "index_attachments_on_attachable_type", using: :btree
 
   create_table "authorizations", force: :cascade do |t|
     t.integer  "user_id"
@@ -37,24 +35,24 @@ ActiveRecord::Schema.define(version: 20160923132025) do
     t.datetime "last_sign_in_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["last_sign_in_at"], name: "index_authorizations_on_last_sign_in_at", using: :btree
+    t.index ["provider"], name: "index_authorizations_on_provider", using: :btree
+    t.index ["user_id"], name: "index_authorizations_on_user_id", using: :btree
   end
 
-  add_index "authorizations", ["last_sign_in_at"], name: "index_authorizations_on_last_sign_in_at", using: :btree
-  add_index "authorizations", ["provider"], name: "index_authorizations_on_provider", using: :btree
-  add_index "authorizations", ["user_id"], name: "index_authorizations_on_user_id", using: :btree
-
   create_table "directions", force: :cascade do |t|
-    t.string   "title",       null: false
-    t.text     "description", null: false
+    t.string   "title",                            null: false
+    t.text     "description",                      null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
     t.string   "slug"
+    t.integer  "steps_count",          default: 0, null: false
+    t.integer  "finished_steps_count", default: 0, null: false
+    t.index ["description"], name: "index_directions_on_description", using: :btree
+    t.index ["title"], name: "index_directions_on_title", using: :btree
+    t.index ["user_id"], name: "index_directions_on_user_id", using: :btree
   end
-
-  add_index "directions", ["description"], name: "index_directions_on_description", using: :btree
-  add_index "directions", ["title"], name: "index_directions_on_title", using: :btree
-  add_index "directions", ["user_id"], name: "index_directions_on_user_id", using: :btree
 
   create_table "steps", force: :cascade do |t|
     t.integer  "direction_id",                 null: false
@@ -63,11 +61,10 @@ ActiveRecord::Schema.define(version: 20160923132025) do
     t.boolean  "is_done",      default: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["direction_id"], name: "index_steps_on_direction_id", using: :btree
+    t.index ["is_done"], name: "index_steps_on_is_done", using: :btree
+    t.index ["title"], name: "index_steps_on_title", using: :btree
   end
-
-  add_index "steps", ["direction_id"], name: "index_steps_on_direction_id", using: :btree
-  add_index "steps", ["is_done"], name: "index_steps_on_is_done", using: :btree
-  add_index "steps", ["title"], name: "index_steps_on_title", using: :btree
 
   create_table "system_logs", force: :cascade do |t|
     t.integer  "user_id",                 null: false
@@ -75,10 +72,9 @@ ActiveRecord::Schema.define(version: 20160923132025) do
     t.jsonb    "data",       default: {}, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["data"], name: "index_system_logs_on_data", using: :gin
+    t.index ["operation"], name: "index_system_logs_on_operation", using: :btree
   end
-
-  add_index "system_logs", ["data"], name: "index_system_logs_on_data", using: :gin
-  add_index "system_logs", ["operation"], name: "index_system_logs_on_operation", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",           null: false
@@ -90,9 +86,8 @@ ActiveRecord::Schema.define(version: 20160923132025) do
     t.string   "last_name"
     t.text     "description"
     t.string   "location"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["nick"], name: "index_users_on_nick", unique: true, using: :btree
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["nick"], name: "index_users_on_nick", unique: true, using: :btree
 
 end
