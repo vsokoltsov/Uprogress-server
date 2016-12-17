a37ee3ef12b11bae3cf43ca84c54d3b4c90c4aaa
Vadim Sokoltsov <vforvad@gmail.com>
2016-12-17 23:43:26 +0300

    add a little bit refactoring to scope objects

--- app/models/scope/user_directions.rb
+++ app/models/scope/user_directions.rb
@@ -7,19 +7,19 @@ class Scope::UserDirections
   end
 
   def finished_directions
-    Direction.joins(:steps).where(user_id: @user.id).uniq!.select do |attr|
+    Direction.joins(:steps).where(user_id: user.id).uniq!.select do |attr|
       steps_status = attr.steps.map(&:is_done)
       steps_status.uniq.size == 1 && steps_status.first
     end.first(5)
   end
 
   def new_directions
-    Direction.joins(:steps).where(user_id: @user.id)
+    Direction.joins(:steps).where(user_id: user.id)
              .group('directions.id, steps.id').having('COUNT(steps) > 0')
              .uniq.limit(5)
   end
 
   def directions
-    Direction.where(user_id: @user.id)
+    Direction.where(user_id: user.id)
   end
 end
