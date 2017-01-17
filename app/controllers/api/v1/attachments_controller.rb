69156e0b7980b41cb3b2b3d9a151b20439231813
Vadim Sokoltsov <vforvad@gmail.com>
2017-01-18 00:30:23 +0300

    fix attachments_controller

--- app/controllers/api/v1/attachments_controller.rb
+++ app/controllers/api/v1/attachments_controller.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 class Api::V1::AttachmentsController < Api::ApiController
   def create
-    form = Form::Attachment.new(Attachment.new, params.to_unsafe_hash)
+    form = Form::Attachment.new(Attachment.new, attachment_attributes)
     if form.submit
       render json: form.object, serializer: AttachmentSerializer, status: :ok
     else
       render json: form.errors.as_json, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def attachment_attributes
+    {
+      file: params[:file],
+      attachable_type: params[:attachable_type],
+      attachable_id: params[:attachable_id]
+    }
   end
 end
--- config/docker/run_server.sh
+++ config/docker/run_server.sh
@@ -1,4 +1,3 @@
 rake db:create
 rake db:migrate
-rake db:seed
 ./bin/rails server -p 3000 -b 0.0.0.0
