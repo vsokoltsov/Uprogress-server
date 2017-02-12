af791d73ab65bcf1961468fda7c46da48c515331
Vadim Sokoltsov <vforvad@gmail.com>
2017-02-12 07:21:14 +0300

    add field to directions serializer

--- app/serializers/directions_serializer.rb
+++ app/serializers/directions_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class DirectionsSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :steps_count,
-             :finished_steps_count, :percents_result, :updated_at
+             :finished_steps_count, :percents_result, :updated_at, :slug
 
   delegate :percents_result, to: :object
 end
