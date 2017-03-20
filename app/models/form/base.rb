7737faf8de4ff588124d52a1d8730e8022b4a26a
Vadim Sokoltsov <vforvad@gmail.com>
2017-03-20 10:33:55 +0300

    add TransactioLock class to Form::Base

--- app/models/form/base.rb
+++ app/models/form/base.rb
@@ -21,16 +21,14 @@ class Form::Base
   def submit(message = nil, &block)
     return unless valid?
     object.assign_attributes(attributes)
-    ActiveRecord::Base.with_advisory_lock(message ? message : object.class.to_s) do
-      ActiveRecord::Base.transaction do
-        object.save!
-        block.call if block_given?
-        create_logs
-        true
-      end
+    ::Service::TransactionLock.run_with_message message ? message : object.class.to_s do
+      object.save!
+      block.call if block_given?
+      create_logs
+      true
     end
   end
-  
+
   private
 
   def create_logs
