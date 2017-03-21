cd98d9567702a62d3941cfb164fd92b45094fa88
Vadim Sokoltsov <vforvad@gmail.com>
2017-03-21 12:33:54 +0300

    implement saving device token to device object

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class Authorization < ActiveRecord::Base
   belongs_to :user
-  has_many :devices
+  has_one :devices
 
   def self.decode_jwt_and_find(token)
     find(JWT.decode(token, nil, false).first['id']) if token
