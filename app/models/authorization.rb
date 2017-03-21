4e8c8582c967bd1ce65adb235f5d60e264c993e8
Vadim Sokoltsov <vforvad@gmail.com>
2017-03-21 11:39:07 +0300

    add devices table

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 class Authorization < ActiveRecord::Base
   belongs_to :user
+  has_many :devices
 
   def self.decode_jwt_and_find(token)
     find(JWT.decode(token, nil, false).first['id']) if token
