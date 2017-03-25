ed4b814d0d3567330cb3da1e8a6d519e1a8be1ad
Vadim Sokoltsov <vforvad@gmail.com>
2017-03-25 12:13:49 +0300

    add some fixes for deploy

--- config/docker/run_server.sh
+++ config/docker/run_server.sh
@@ -1,3 +1,4 @@
 rake db:create
 rake db:migrate
-./bin/rails server -p 3000 -b 0.0.0.0
+./bin/bundle exec rake tmp:clear
+rm -f /var/www/docker_example/tmp/pids/server.pid && ./bin/bundle exec rails server -p 3000 -b 0.0.0.0
