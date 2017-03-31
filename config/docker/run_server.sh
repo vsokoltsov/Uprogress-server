4ac12682a73c4b2c78f22965bba3f7d95c1683c2
Vadim Sokoltsov <vforvad@gmail.com>
2017-04-01 01:36:16 +0300

    try to implement sidekiq work inside rails. Failed

--- config/docker/run_server.sh
+++ config/docker/run_server.sh
@@ -1,7 +1,8 @@
 rake db:create
 rake db:migrate
 ./bin/bundle exec rake tmp:clear
-rm -f /var/www/uprogress/tmp/pids/server.pid
-./bin/bundle install
+if [ -f "/var/www/docker_example/tmp/pids/server.pid" ]; then
+  echo "DELETE SERVER_PID"
+  rm -f /var/www/docker_example/tmp/pids/server.pid
+fi
 ./bin/bundle exec rails server -p 3000 -b 0.0.0.0
-bundle exec sidekiq -d -L log/sidekiq.log  -e production
