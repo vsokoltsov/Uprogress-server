bf25147f0b88853308da20b0fc5f857b6df768b9
Vadim Sokoltsov <vforvad@gmail.com>
2017-04-05 20:11:22 +0300

    add specs for devices_controller

--- app/controllers/api/v1/devices_controller.rb
+++ app/controllers/api/v1/devices_controller.rb
@@ -3,13 +3,19 @@ class Api::V1::DevicesController < Api::ApiController
   before_action :validate_token
 
   def create
-    form = Form::Device.new(
-      params[:device].merge!(authorizaiton_id: current_authorization.id)
-    )
+    form = Form::Device.new(device_attirbutes)
     if form.submit
       render json: { success: true }
     else
       render json: { errors: form.errors }, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def device_attirbutes
+    params[:device]&.to_unsafe_hash
+          &.merge!(authorization_id: current_authorization.id)
+          &.symbolize_keys || {}
   end
 end
