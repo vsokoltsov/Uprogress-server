ae480401c5859db0a845eb5295bb4554bcaa3bdf
Vadim Sokoltsov <vforvad@gmail.com>
2017-04-08 15:48:27 +0300

    add specs for Service::Appointment::Never class

--- spec/models/service/appointment/never_spec.rb
+++ spec/models/service/appointment/never_spec.rb
@@ -4,23 +4,45 @@ require 'rails_helper'
 describe Service::Appointment::Never do
   let!(:user) { create :user }
   let!(:direction) { create :direction, user_id: user.id }
-  let!(:appointment) { create :appointment, direction_id: direction.id }
-  let!(:appointment_service) { Service::Appointment::Never.new(appointment) }
 
   describe 'send_notification' do
     context 'with valid attributes' do
-      it 'call send_notificatio method on user' do
+      let!(:appointment) { create :appointment, direction_id: direction.id }
+      let!(:appointment_service) { Service::Appointment::Never.new(appointment) }
+
+      before do
+        ::Service::PushNotifications.any_instance.stub(:send_notification)
+                                    .with(any_args).and_return(true)
+      end
+
+      it 'call send_notification method on user' do
         expect_any_instance_of(
           ::Service::PushNotifications
         ).to receive(:send_notification).with(any_args).and_return(true)
-        ::Service::PushNotifications.any_instance.stub(:send_notification)
-                                    .with(any_args).and_return(true)
+
         appointment_service.send_notification
+      end
+
+      it 'updates appointment' do
+        appointment_service.send_notification
+        appointment.reload
+        expect(appointment.available).to be_falsy
       end
     end
 
     context 'with invalid attributes' do
+      let!(:appointment) do
+        create :appointment, direction_id: direction.id, available: false
+      end
+      let!(:appointment_service) { Service::Appointment::Never.new(appointment) }
 
+      it 'send notification does not called' do
+        expect_any_instance_of(
+          ::Service::PushNotifications
+        ).to receive(:send_notification).with(any_args).and_return(false)
+
+        appointment_service.send_notification
+      end
     end
   end
 end
