7894a71736596c631b732ad3e20ae598b89aace8
Vadim Sokoltsov <vforvad@gmail.com>
2017-04-10 12:15:51 +0300

    add direction_appointment_serializer and add some fixes to existed one

--- app/serializers/direction_serializer.rb
+++ app/serializers/direction_serializer.rb
@@ -6,6 +6,7 @@ class DirectionSerializer < ActiveModel::Serializer
   delegate :percents_result, to: :object
   has_many :steps, serializer: StepsSerializer
   has_one :user, serializer: DirectionUserSerializer
+  has_many :appointments, serializer: DirectionAppointmentSerializer
 
   def steps
     object.steps.order(:created_at)
