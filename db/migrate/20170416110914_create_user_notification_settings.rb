587ca39e89c284b314e0d0767061e5841f07397f
Vadim Sokoltsov <vforvad@gmail.com>
2017-04-16 14:22:18 +0300

    add migration, model and relations to notification_setting object

---
+++ db/migrate/20170416110914_create_user_notification_settings.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+class CreateUserNotificationSettings < ActiveRecord::Migration[5.0]
+  def change
+    create_table :notification_settings do |t|
+      t.belongs_to :user, index: true, null: false
+      t.boolean :push_enabled, default: true, null: false
+      t.boolean :mail_enabled, default: true, null: false
+      t.timestamps
+    end
+  end
+end
